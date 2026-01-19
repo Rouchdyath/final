fc61bc9db39884e7d75c112eb12214a69c093d53
Rouchdyath <rouchdyatholas@gmail.com>
2026-01-19 11:46:32 +0100

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,8 +32,7 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "*",
 ]
 
 # Application definition
