a0c32cfddc863aa072878d9a97e65888b3122d13
Rouchdyath Ola <rouchdyatholas@gmail.com>
2026-01-19 01:44:52 +0100

    créaction du superuser

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ cd backend
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Créer un superutilisateur automatiquement si il n'existe pas
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'Motdepasse123!')" | python manage.py shell
