bdb5eb90aebea1612c8e8b85cb66c9f888e34289
Rouchdyath <rouchdyatholas@gmail.com>
2026-01-19 12:40:19 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Créer un superutilisateur automatiquement si il n'existe pas
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'Motdepasse123!')" | python manage.py shell
+# Créer un superutilisateur si il n'existe pas
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@edusphere.com', 'Admin2026!')" | python manage.py shell
